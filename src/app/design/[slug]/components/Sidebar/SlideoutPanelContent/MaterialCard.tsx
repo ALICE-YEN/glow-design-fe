d870de81cc7ce026cb25583a9600d37f2464a0f9
Alice Yen <gp40424@gmail.com>
2025-06-05 17:44:21 +0800

    feat: MaterialCard PlaceholderImg onError

--- src/app/design/[slug]/components/Sidebar/SlideoutPanelContent/MaterialCard.tsx
+++ src/app/design/[slug]/components/Sidebar/SlideoutPanelContent/MaterialCard.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 import type { Material } from "@/app/design/[slug]/types/interfaces";
+import PlaceholderImg from "@/assets/imgs/placeholder.png";
 
 interface MaterialCardProps {
   material: Material;
@@ -12,19 +14,22 @@ export default function MaterialCard({
   material,
   handleClick,
 }: MaterialCardProps) {
+  const [imgSrc, setImgSrc] = useState<string>(material.url);
+
   const isFurniture = material.url.includes("/furniture/");
 
   return (
     // 樣式放在 design.css
     <div className="card cursor-pointer" onClick={() => handleClick(material)}>
       <Image
-        src={material.url}
+        src={imgSrc}
         alt={material.name}
         width={140}
         height={140}
         className={`card__img ${
           isFurniture ? "object-contain" : "object-cover"
         }`}
+        onError={() => setImgSrc(PlaceholderImg.src)}
       />
       <div className="card__content">
         <div className="card__content__header">{material.name}</div>
